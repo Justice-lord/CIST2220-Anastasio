a9c6cfd907358a031574bfb2714181cb5fc9fbdb
Justice <tanastasio@student.cnm.edu>
2026-05-30 22:58:10 -0600

    Ideal gas init

---
+++ IdealGasCalculator/IdealGasCalculator/Program.cs
@@ -0,0 +1,84 @@
+﻿/* Programmer: Tucker Anastasio (tanastasio@student.cnm.edu)
+ * Program: Ideal Gas Calculator
+ * Date: 5/30/2026*/
+
+class IdealGasCalculator
+{
+    // header creation and format
+    static void WriteHeader()
+    {
+        Console.WriteLine("Programmer: Tucker Anastasio\nProgram: Ideal Gas Calculator\n" +
+            "Objective: Find pressure using the ideal gas equation.\n");
+    }
+
+    // load csv data into arrays
+    static void GetMoleWeights(string filePath, out string[] gasNames, out double[] moleWeights, out int count)
+    {
+        // using lists to not force an array size
+        List<string> names = new List<string>();
+        List<double> moles = new List<double>();
+
+        // open and read the file <filepath>
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            // skip first line of data (column names)
+            reader.ReadLine();
+
+            string line;
+            count = 0;
+
+            // read line by line until the end of data
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+
+                // add values pairs into lists
+                names.Add(parts[0]);
+                moles.Add(double.Parse(parts[1]));
+
+                // track number of entries in data
+                count++;
+            }
+        }
+
+        // list to arrays for out
+        gasNames = names.ToArray();
+        moleWeights = moles.ToArray();
+    }
+
+    private static void DisplayGasNames(string[] gasNames, int countGases)
+    {
+        // sets number of entries per row
+        for (int i = 0; i < countGases; i +=3)
+        {
+            // adds entries to rows
+            for (int j = i; j < i +3 && j < countGases; j++)
+            {
+                Console.Write($"{gasNames[j],-20}");
+            }
+
+            // moves to next row
+            Console.WriteLine();
+        }
+    }
+
+    //private static double GetMoleWeightFromName(string gasName, string[] gasNames, double[] moleWeights, int countGases)
+    
+
+    
+    
+    static void Main(string[] args)
+    {
+        WriteHeader();
+
+        // variables for GetMoleWeights
+        string filePath = "MolecularWeightsGasesAndVapors.csv";
+        string[] gasNames;
+        double[] moleWeights;
+        int count;
+
+        GetMoleWeights(filePath, out gasNames, out moleWeights, out count);
+
+        DisplayGasNames(gasNames, count);
+    }
+}
