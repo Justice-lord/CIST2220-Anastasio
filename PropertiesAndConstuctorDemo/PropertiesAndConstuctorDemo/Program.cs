2a02e4f592b37a3dd400d65b45870b5742c8a2e2
Justice <tanastasio@student.cnm.edu>
2026-06-08 11:14:08 -0600

    Final Commit 6/8/2026

---
+++ PropertiesAndConstuctorDemo/PropertiesAndConstuctorDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAndConstuctorDemo
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            Student myStudent = new Student("S001", "John", "Smith");
+            Console.WriteLine(myStudent.ToString());
+
+            string doAnother;
+            do
+            {
+                Student st = new Student();
+                st.Firstname = GetInput("First Name");
+                st.Lastname = GetInput("Last Name");
+                st.Major = GetInput("Major");
+                st.StudentNumber = GetInput("Student Number");
+                st.Score1 = int.Parse(GetInput("Score 1"));
+                st.Score2 = int.Parse(GetInput("Score 2"));
+                st.Score3 = int.Parse(GetInput("Score 3"));
+                students.Add(st);
+
+                doAnother = GetInput("\nAdd another student? (y/n)");
+            } while (doAnother == "y");
+
+            Console.WriteLine("Student Averages: ");
+            foreach (Student st in students)
+            {
+                Console.WriteLine(st.ToString());
+            }
+        }
+
+        private static string GetInput(string prompt)
+        {
+            Console.Write(prompt + ": ");
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ PropertiesAndConstuctorDemo/PropertiesAndConstuctorDemo/Student.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAndConstuctorDemo
+{
+    internal class Student
+    {
+        public static string[] majors = new string[] { "Programming", "Business", "Art History" };
+        // fields private
+        private int score1;
+        private int score2;
+        private int score3;
+
+        // properties
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string StudentNumber { get; set; }
+        public string Major { get; set; }
+        public double Average { get; private set; }
+
+        public int Score1
+        {
+            get { return score1; }
+            set
+            {
+                score1 = value;
+                Calc();
+            }
+        }
+        public int Score2
+        {
+            get { return score2; }
+            set
+            {
+                score2 = value;
+                Calc();
+            }
+        }
+        public int Score3
+        {
+            get { return score3; }
+            set
+            {
+                score3 = value;
+                Calc();
+            }
+        }
+
+        public override string ToString()
+        {
+            return
+                Firstname + " " + Lastname
+                + " Major: " + Major
+                + " Average: " + Average;
+        }
+
+        private void Calc()
+        {
+            Average = (score1 + score2 + score3) / 3;
+        }
+
+        // constructors
+        public Student() : this("Number Pending", "TBD", "TBD", -1, -1, -1, "Undeclared")
+        {
+        }
+
+        public Student(String sID)
+            : this(sID, "TBD", "TBD", -1, -1, -1, "Undeclared")
+        {
+        }
+
+        public Student(string sID, string firstName, string lastName)
+            : this(sID, firstName, lastName, -1, -1, -1, "Undeclared")
+        {
+        }
+
+        public Student(string sID, string firstName, string lastName, int s1, int s2, int s3, string maj)
+        {
+            StudentNumber = sID;
+            Firstname = firstName;
+            Lastname = lastName;
+            Score1 = s1;
+            Score2 = s2;
+            Score3 = s3;
+            Major = maj;
+        }
+
+        /* destructor
+        ~Student()
+        {
+            Console.WriteLine($"Student {StudentNumber} data no longer in memeory!");
+        }
+
+        // constuctors
+        public Student()
+        {
+            StudentNumber = "Number Pending";
+            Firstname = "TBD";
+            Lastname = "TBD";
+            Major = "Undeclared";
+        }
+
+        public Student(string sID)
+        {
+            StudentNumber = sID;
+            Firstname = "TBD";
+            Lastname = "TBD";
+            Major = "Undeclared";
+        }
+
+        public Student(string sID, string firstName, string lastName)
+        {
+            StudentNumber = sID;
+            Firstname= firstName;
+            Lastname= lastName;
+            Major = "Undeclared";
+        }
+
+        public Student(string sID, string firstName, string lastName, int s1, int s2, int s3, string maj)
+        {
+            StudentNumber = sID;
+            Firstname = firstName;
+            Lastname = lastName;
+            Score1 = s1;
+            Score2 = s2;
+            Score3 = s3;
+            Major = maj;
+        }*/
+       
+    }
+}
