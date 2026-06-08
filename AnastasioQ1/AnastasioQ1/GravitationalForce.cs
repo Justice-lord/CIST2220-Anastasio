44b98d7a83283dfa127ccf6dcf2de9827a5e9469
Justice <tanastasio@student.cnm.edu>
2026-06-08 14:44:22 -0600

    Final Commit 6/8/2026

---
+++ AnastasioQ1/AnastasioQ1/GravitationalForce.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnastasioQ1
+{
+    internal class GravitationalForce
+    {
+        // fields
+        private double gravConstant = 6.67408e-11;
+        private double mass1;
+        private double mass2;
+        private double distance = 1;
+
+        // default constructor
+        public GravitationalForce()
+        {
+
+        }
+
+        // properties
+        public string Name1 {get; set;}
+        public string Name2 {get; set;}
+        public double GravForce { get; private set;}
+
+        public double Mass1
+        {
+            get { return mass1; }
+            set
+            {
+                mass1 = value;
+                Calc();
+            }
+        }
+
+        public double Mass2
+        {
+            get { return mass2; }
+            set
+            {
+                mass2 = value;
+                Calc();
+            }
+        }
+
+        public double Distance
+        {
+            get { return distance; }
+            set
+            {
+                distance = value;
+                Calc();
+            }
+        }
+
+        // force calculation
+        private void Calc()
+        {
+            GravForce = gravConstant * ((Mass1 * Mass2) / Math.Pow(Distance, 2));
+        }
+
+        // display results
+        public override string ToString()
+        {
+            return $"\nThe Force between {Name1} and {Name2} is {GravForce:E} newtons.";
+        }
+    }
+}
---
+++ AnastasioQ1/AnastasioQ1/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Programmer: Tucker Anastasio (tanastasio@student.cnm.edu)
+ * Program: Force calc between two objects
+ * Date: 6/8/2026*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnastasioQ1
+{
+    class Program
+    {
+        // helper functions
+        private static string GetInput(string prompt)
+        {
+            Console.Write($"\n{prompt}: ");
+            return Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            string doAnother;
+            do
+            {
+                // user input
+                GravitationalForce UserForce = new GravitationalForce();
+
+                UserForce.Name1 = GetInput("Name of Object 1");
+                UserForce.Mass1 = double.Parse(GetInput($"Mass of {UserForce.Name1} in kg"));
+                UserForce.Name2 = GetInput("Name of Object 2");
+                UserForce.Mass2 = double.Parse(GetInput($"Mass of {UserForce.Name2} in kg"));
+                UserForce.Distance = double.Parse(GetInput($"Distance between {UserForce.Name1} and {UserForce.Name2} in m"));
+
+                // display results
+                Console.WriteLine(UserForce.ToString());
+
+
+                Console.Write("\nDo another? (Y/n)");
+                doAnother = Console.ReadLine();
+
+            } while (doAnother.ToUpper() == "Y");
+
+            Console.WriteLine("\nThanks for using my program");
+        }
+    }
+}
