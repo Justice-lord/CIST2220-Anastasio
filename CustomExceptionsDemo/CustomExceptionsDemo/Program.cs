5f7a7be7645d7d5e2880d2b5798170dbc23bbf3b
Justice <tanastasio@student.cnm.edu>
2026-06-14 11:12:31 -0600

    Final Commit 6/14/2026

---
+++ CustomExceptionsDemo/CustomExceptionsDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CustomExceptionsDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string doAnother;
+            do
+            {
+                try
+                {
+                    Console.Write("Please enter a social security number: ");
+                    string userInput = Console.ReadLine();
+                    SocialSecurityNumber ssn = new SocialSecurityNumber();
+                    ssn.SSN = userInput;
+
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine($"Error! {exc.Message}\nException type: {exc.GetType()}");
+                }
+                finally
+                {
+                    Console.WriteLine("This section always executes!");
+                }
+
+                Console.Write("Do another (y/n): ");
+                doAnother = Console.ReadLine();
+            } while (doAnother.ToLower() == "y");
+        }
+    }
+}
