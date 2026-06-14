bc5d284d5063dfc63ea41855638aa8fa70f62364
Justice <tanastasio@student.cnm.edu>
2026-06-13 20:18:33 -0600

    Final Commit 6/13/2026

---
+++ HRManager/HRManager/HourlyEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManager
+{
+    internal class HourlyEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; }
+
+        public HourlyEmployee() : base()
+        {
+            HourlyRate = 15.0M;
+        }
+
+        public override string PaySummary
+        {
+            get { return $"This employee is payed {HourlyRate} per hour"; }
+        }
+    }
+}
---
+++ HRManager/HRManager/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HRManager
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.EmpNum = 1;
+            employee.FirstName = "Steve";
+            employee.LastName = "Jobs";
+
+            HourlyEmployee hourEmployee = new HourlyEmployee();
+            hourEmployee.EmpNum = 2;
+            hourEmployee.FirstName = "Bill";
+            hourEmployee.LastName = "Gates";
+            hourEmployee.HourlyRate = 15.00M;
+
+            Console.WriteLine($"emp.GetPaySummary: {employee.PaySummary}");
+            Console.WriteLine($"hourEmpolyee.GetPaySummary: {hourEmployee.PaySummary}");
+
+            Employee employee2 = hourEmployee;
+
+            Console.WriteLine($"emp2.GetPaySummary: {employee2.PaySummary}");
+        }
+    }
+}
