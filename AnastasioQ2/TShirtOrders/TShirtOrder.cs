d2adc70a763ae808f996cd95b45b5ad1371610ea
Justice <tanastasio@student.cnm.edu>
2026-06-14 19:52:43 -0600

    Final Commit for Q2 6/14/2026

---
+++ AnastasioQ2/TShirtOrders/TShirtOrder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+//TShirtOrder.cs
+//Programmer: Rob Garner (rgarner7@cnm.edu)
+//Date: 10 Mar 2020
+//Purpose: Model a TShirt order.
+namespace TShirtOrders
+{
+    /// <summary>
+    /// TShirtOrder
+    /// Provides a model of a shirt order.
+    /// </summary>
+    public class TShirtOrder
+    {
+        // TA moved required param to front made it optional
+        public TShirtOrder(double printAreaInSqIn = 0, string firstName="",string lastName = "", string address = "", DateTime? orderDate=null,bool isLocalPickup=true,  int numColors=1,int numShirts=1)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            OrderDate = orderDate;
+            IsLocalPickup = isLocalPickup;
+            this.printAreaInSqIn = printAreaInSqIn;
+            this.numColors = numColors;
+            this.numShirts = numShirts;
+            Calc();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public bool IsLocalPickup { get; set; }
+        private double printAreaInSqIn;
+        public double PrintAreaInSqIn
+        {
+            get { return printAreaInSqIn; }
+            set { printAreaInSqIn = value; Calc(); }
+        }
+
+        private int numColors;
+        public int NumColors
+        {
+            get { return NumColors; }
+            set { NumColors = value; Calc(); }
+        }
+
+        private int numShirts;
+        public int NumShirts
+        {
+            get { return numShirts; }
+            set { numShirts = value; Calc(); }
+        }
+        // TA switched to private set instead of get
+        public decimal Price { get; private set; }
+        private void Calc()
+        {
+            // TA fixed type mismatch
+            Price = numShirts * ((decimal)numColors * 2.25m + (decimal)printAreaInSqIn * .05m);
+        }
+        public override string ToString()
+        {
+            return FirstName+" "
+                +LastName+" "
+                // TA checking for nullable
+                +OrderDate?.ToString("MM/dd/yyyy HH:mm:ss")+" "
+                +" Price: "+Price.ToString("c");
+        }
+    }
+}
