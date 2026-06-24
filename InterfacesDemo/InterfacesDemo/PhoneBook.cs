03e7514f74c74c4704af7a7c06a88b78e4346cb6
Justice <tanastasio@student.cnm.edu>
2026-06-24 16:31:41 -0600

    Final Commit 6/24/2026

---
+++ InterfacesDemo/InterfacesDemo/PhoneBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    class PhoneBook
+    {
+        public List<IPhoneBookItem> PhoneBookEntries { get; set; }
+
+        public PhoneBook()
+        {
+            PhoneBookEntries = new List<IPhoneBookItem>();
+        }
+
+        public string GetPhoneBook()
+        {
+            string phoneBook = "";
+            foreach (var item in PhoneBookEntries)
+            {
+                phoneBook += item.GetContactSummary() + "\n\n";
+            }
+            return phoneBook;
+        }
+    }
+}
