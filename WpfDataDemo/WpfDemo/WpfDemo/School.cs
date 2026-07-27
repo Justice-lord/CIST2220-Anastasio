50ba904d3f19fc71cfd7c997c722c6fb2330c5fd
Justice <tanastasio@student.cnm.edu>
2026-07-27 11:09:06 -0600

    Final Commit 7/27/2026

---
+++ WpfDataDemo/WpfDemo/WpfDemo/School.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace WPFDemo
+{
+    class School
+    {
+        private List<string> course;
+        private List<string> major;
+        private List<string> campus;
+        private List<Student> student;
+
+        public List<string> Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+
+        public List<string> Major
+        {
+            get { return major; }
+            set { major = value; }
+        }
+
+        public List<string> Campus
+        {
+            get { return campus; }
+            set { campus = value; }
+        }
+
+        public List<Student> Student
+        {
+            get { return student; }
+            set { student = value; }
+        }
+
+        public School()
+        {
+            course = GetData("SELECT * FROM Courses");
+            campus = GetData("SELECT * FROM Campus");
+            major = GetData("SELECT * FROM Major");
+        }
+
+        private List<string> GetData(string sqlStr)
+        {
+            List<string> items = new List<string>();
+            string connStr = ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                SqlCommand selectCmd = new SqlCommand(sqlStr, conn);
+                conn.Open();
+                SqlDataReader dr = selectCmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    items.Add(dr.GetString(1));
+                }
+            }
+            return items;
+        }
+    }
+
+}
---
+++ WpfDataDemo/WpfDemo/WpfDemo/SchoolWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for SchoolWindow.xaml
+    /// </summary>
+    public partial class SchoolWindow : Window
+    {
+        School school;
+        public SchoolWindow()
+        {
+            InitializeComponent();
+            school = new School();
+            lbCampuses.ItemsSource = school.Campus;
+            lbCourses.ItemsSource = school.Course;
+            lbMajor.ItemsSource = school.Major;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Student newStudent = new Student();
+            StudentWindow studentWindow = new StudentWindow(newStudent);
+
+            //StudentWindow studentWindow = new StudentWindow();
+            //Student newStudent = studentWindow.Student;
+            studentWindow.ShowDialog();
+            if(studentWindow.DialogResult == true)
+            {
+                lbStudents.Items.Add(newStudent);
+            }
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lblCampus.Content = lbCampuses.SelectedItem.ToString()
+                ;
+        }
+    }
+}
