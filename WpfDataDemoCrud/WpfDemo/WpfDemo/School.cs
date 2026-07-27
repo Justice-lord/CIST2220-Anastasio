3e0490c773036f20e8aecab497b7eef299ae7fea
Justice <tanastasio@student.cnm.edu>
2026-07-27 14:55:17 -0600

    Final Commit 7/27/2026

---
+++ WpfDataDemoCrud/WpfDemo/WpfDemo/School.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WPFDemo
+{
+    class School
+    {
+        private DataTable course;
+        private DataTable major;
+        private DataTable campus;
+        private List<string> student;
+
+        public DataTable Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+
+        public DataTable Major
+        {
+            get { return major; }
+            set { major = value; }
+        }
+
+        public DataTable Campus
+        {
+            get { return campus; }
+            set { campus = value; }
+        }
+
+        public List<string> Student
+        {
+            get { return student; }
+            set { student = value; }
+        }
+
+        public School()
+        {
+            Refresh();
+        }
+
+        private DataTable GetData(string sqlStr)
+        {
+            DataTable dt = new DataTable();
+            string connStr = ConfigurationManager.ConnectionStrings["SchoolDb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                SqlCommand selectCmd = new SqlCommand(sqlStr, conn);
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter(selectCmd);
+                da.FillSchema(dt, SchemaType.Source);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public void Refresh()
+        {
+            campus = GetData("SELECT * FROM Campus");
+            course = GetData("SELECT * FROM Course");
+            major = GetData("SELECT * FROM Major");
+        }
+    }
+
+}
---
+++ WpfDataDemoCrud/WpfDemo/WpfDemo/SchoolWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for SchoolWindow.xaml
+    /// </summary>
+    public partial class SchoolWindow : Window
+    {
+        School school;
+        public SchoolWindow()
+        {
+            InitializeComponent();
+            school = new School();
+            lbCampuses.DisplayMemberPath = "Name";
+            lbCampuses.ItemsSource = school.Campus.DefaultView;
+            lbCourses.DisplayMemberPath = "Title";
+            lbCourses.ItemsSource = school.Campus.DefaultView;
+            lbMajor.DisplayMemberPath = "Title";
+            lbMajor.ItemsSource = school.Major.DefaultView;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Student newStudent = new Student();
+            StudentWindow studentWindow = new StudentWindow(newStudent);
+
+            //StudentWindow studentWindow = new StudentWindow();
+            //Student newStudent = studentWindow.Student;
+            studentWindow.ShowDialog();
+            if(studentWindow.DialogResult == true)
+            {
+                lbStudents.Items.Add(newStudent);
+            }
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //lblCampus.Content = lbCampuses.SelectedItem.ToString()
+                ;
+        }
+
+        private void btnCampusAddUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (lbCampuses.SelectedItem == null)
+            {
+                string insStr = "INSERT INTO Campus(Name) VALUES(@Name)";
+                string connStr = ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    SqlCommand insCmd = new SqlCommand(insStr, conn);
+                    insCmd.Parameters.AddWithValue("Name", tbxCampus.Text);
+                    conn.Open();
+                    insCmd.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                string updStr = "UPDATE Campus SET Name = @Name WHERE CampusID = @CampusID";
+                string connStr = ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString;
+                using (SqlConnection conn =new SqlConnection(connStr))
+                {
+                    SqlCommand updCmd = new SqlCommand(updStr, conn);
+                    updCmd.Parameters.AddWithValue("Name", tbxCampus.Text);
+                    int campusId = (int)((DataRowView)lbCampuses.SelectedItem)["CampusID"];
+                    updCmd.Parameters.AddWithValue("CampusID", campusId);
+                    conn.Open();
+                    updCmd.ExecuteNonQuery();
+                }
+            }
+
+            school.Refresh();
+            lbCampuses.ItemsSource = school.Campus.DefaultView;
+            lbCampuses.Items.Refresh();
+        }
+
+        private void btnCampusDelete_Click(object sender, EventArgs e)
+        {
+            if (lbCampuses.SelectedIndex != null)
+            {
+                string delStr = "DELETE FROM Campus WHERE CampusID = @CampusID";
+                string connStr = ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString;
+                using (SqlConnection conn =  new SqlConnection(connStr))
+                {
+                    SqlCommand delCmd = new SqlCommand(delStr, conn);
+                    int campusID = (int)((DataRowView)lbCampuses.SelectedItem)["CampusID"];
+                    delCmd.Parameters.AddWithValue("CampusID", campusID);
+                    conn.Open();
+                    delCmd.ExecuteNonQuery();
+                }
+
+                school.Refresh();
+                lbCampuses.ItemsSource = school.Campus.DefaultView;
+                lbCampuses.Items.Refresh();
+            }
+        }
+    }
+}
